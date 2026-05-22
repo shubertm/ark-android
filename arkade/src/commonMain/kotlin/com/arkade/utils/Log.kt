dc12e9aeedfe2aa2ce9d9da3d9f0c2cf614ef4b0
Shubert Munthali <shubertm.m@gmail.com>
2026-05-22 22:40:25 +0200

    refactor: JVM logger (#58)
    * JVM logger
    
    * Apply fixes

--- arkade/src/commonMain/kotlin/com/arkade/utils/Log.kt
+++ arkade/src/commonMain/kotlin/com/arkade/utils/Log.kt
@@ -26,3 +26,24 @@ expect fun Log.verbose(
     tag: String,
     message: String,
 )
+
+fun Log.drawLine() {
+    println("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+    skipLine()
+}
+
+fun Log.skipLine() {
+    println()
+}
+
+fun Log.success(
+    tag: String,
+    message: String,
+    indent: Int = 0,
+) {
+    var indentString = ""
+    for (i in 0..indent) {
+        indentString = indentString.plus("  ")
+    }
+    info(tag, "\u001B[32m$indentString✓ PASSED: $message")
+}
