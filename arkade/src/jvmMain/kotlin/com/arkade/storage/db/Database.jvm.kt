4f507342e01e3d1d5b5c8ac59e5a40543a310118
Shubert Munthali <shubertm.m@gmail.com>
2026-06-04 16:11:08 +0200

    Apply fixes

--- arkade/src/jvmMain/kotlin/com/arkade/storage/db/Database.jvm.kt
+++ arkade/src/jvmMain/kotlin/com/arkade/storage/db/Database.jvm.kt
@@ -7,6 +7,9 @@ import java.io.File
 fun initializeRoomDatabaseBuilder(appName: String): RoomDatabase.Builder<Database> {
     val appDir =
         File(System.getProperty("user.home"), ".$appName").also {
+            if (it.exists() && !it.isDirectory) {
+                throw IllegalArgumentException("App path exists but is not a directory: ${it.absolutePath}")
+            }
             if (!it.exists() && !it.mkdirs()) {
                 throw IllegalArgumentException("Failed to create app directory: ${it.absolutePath}")
             }
