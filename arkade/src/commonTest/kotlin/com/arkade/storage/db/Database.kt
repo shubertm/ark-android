743b5c214e8a8796cf868b74fe40c6ad17f6e7ea
Shubert Munthali <shubertm.m@gmail.com>
2026-06-04 15:59:34 +0200

    Apply fixes

--- arkade/src/commonTest/kotlin/com/arkade/storage/db/Database.kt
+++ arkade/src/commonTest/kotlin/com/arkade/storage/db/Database.kt
@@ -3,10 +3,10 @@ package com.arkade.storage.db
 import androidx.room.RoomDatabase
 
 /**
- * Obtain a Database instance configured for use in tests.
+ * Obtain a Database builder instance configured for use in tests.
  *
- * Platform-specific implementations should provide a test-ready Database.
+ * Platform-specific implementations should provide a test-ready Database builder.
  *
- * @return A `Database` configured for testing (isolated/test data setup).
+ * @return A `RoomDatabase.Builder<Database>` configured for testing (isolated/test data setup).
  */
 expect fun initializeTestDatabaseBuilder(): RoomDatabase.Builder<Database>
