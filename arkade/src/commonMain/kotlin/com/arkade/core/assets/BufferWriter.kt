e84ed4fe0c99dcfc67067991ce219df0dee7d6aa
Shubert Munthali <shubertm.m@gmail.com>
2026-08-07 15:45:47 +0200

    Guard uint16 range in writeUInt16LE

--- arkade/src/commonMain/kotlin/com/arkade/core/assets/BufferWriter.kt
+++ arkade/src/commonMain/kotlin/com/arkade/core/assets/BufferWriter.kt
@@ -3,6 +3,7 @@ package com.arkade.core.assets
 import fr.acinq.bitcoin.io.ByteArrayOutput
 
 fun ByteArrayOutput.writeUInt16LE(value: Int) {
+    require(value in 0..0xFFFF) { "Value out of uint16 range: $value" }
     write(value.toByte().toInt())
     write((value shr 8).toByte().toInt())
 }
